6bfd6572ec9a88d701a7f4ee96ca866e9ff72332
xiso <xiso@amuz.co.kr>
2022-08-02 17:53:58 +0900

    vue with v-if / v-for

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>hi</h1>
+    <input type="text" v-model="inputValue" />
+    <input type="text" v-model="inputCount" />
+    <input type="text" v-model="inputName" />
+    <hr />
+    <h4>{{inputName}} 님의 {{ inputValue }} 값은 {{inputCount}} 개 입니다.</h4>
+    <hr />
+      <div v-for="(cardObj,order) in titles" :key="order">
+        {{ cardObj }}
+        <card-ui
+          :menuTitle="order + ' 번째 반복 - ' + cardObj.title"
+          :menuCount="cardObj.menuCount"
+          :backbroundColor="cardObj.bgColor"
+          />
+      </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// define -> 정의
+import CardUi from './ui-items/card-ui.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      inputValue : 'default value',
+      inputCount : 'default count',
+      inputName : 'default name',
+      titles : [
+        {
+          title : '동해물과 백두산이',
+          menuCount : 10,
+          bgColor : 'red',
+        },
+        {
+          title : '마르고 닳도록',
+          menuCount : 2,
+          bgColor : 'blue',
+        },
+        {
+          title : '동해물과 백두산이',
+          menuCount : 3,
+          bgColor : 'black',
+        },
+        {
+          title : '동해물과 백두산이',
+          menuCount : 2,
+          bgColor : 'green',
+        },
+        {
+          title : '동해물과 백두산이',
+          menuCount : 1,
+          bgColor : 'yellow',
+        },
+      ],
+    }
+  },
+  //등록
+  components : {
+    CardUi,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background:#eee;
 }
 </style>
---
+++ src/ui-items/card-ui.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class='amuz-card'>
+        <h2 v-if="menuTitle">
+            {{ menuTitle }}
+        </h2>
+        <p v-else class="untitled">
+            no title
+        </p>
+        <ul :style="{'background-color':backbroundColor}">
+
+            <li v-for="menu in menuCount" :key="menu">{{menu}}</li>
+
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props :{
+        menuTitle : String,
+        menuCount : Number,
+        backbroundColor : String,
+    },
+    components : {
+
+    }
+}
+</script>
+
+<style>
+.amuz-card{
+    background:#fff;
+    margin:20px 0;
+    border:3px solid #eee;
+    border-radius:10px;
+    padding:20px;
+}
+.untitled{
+    background:#eee; text-align:center;
+}
+</style>
